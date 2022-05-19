b8b2adc175c6e35e07b20648f144f5050f189386
Shubham Raj Pandit <shubhamraj465@gmail.com>
2022-05-19 21:20:15 +0530

    Added Routes in the Body component

--- frontend/src/components/Body.jsx
+++ frontend/src/components/Body.jsx
@@ -1,4 +1,8 @@
+import { Route, Routes } from "react-router-dom"
 import { useState, useEffect } from "react"
+import Home from "./Home"
+import Blog from "./Blog"
+import Review from "./Review"
 
 const Body = () => {
   const [topMargin, setTopMargin] = useState('50px')
@@ -9,7 +13,11 @@ const Body = () => {
   
   return (
     <div style={{marginTop: `${topMargin}px`}}>
-      
+      <Routes>
+        <Route path='/' element={ <Home /> }> </Route>
+        <Route path='/blog' element={ <Blog /> }> </Route>
+        <Route path='/review' element={ <Review /> }> </Route>
+      </Routes>
     </div>
   )
 }
