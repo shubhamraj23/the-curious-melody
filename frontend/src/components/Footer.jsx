f02cb75020d2e1914d1250629c119277460a6391
Shubham Raj Pandit <shubhamraj465@gmail.com>
2022-05-19 17:45:17 +0530

    Updated Footer design

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,88 +1,81 @@
-import { MdEmail, MdOutlineMyLocation, MdWeb, MdMeetingRoom } from "react-icons/md";
+import { MdEmail, MdWeb, MdMeetingRoom } from "react-icons/md";
+import { ImLocation } from "react-icons/im";
 
 const Footer = () => {
   return (
     <div className="bg-emerald-300">
       <div className="py-4">
-        <div className="flex">
-          <div className="w-2/3 md:w-1/2">
-            <div className="px-2">
-              <div>
-                <p className="mx-auto font-semibold text-center text-base md:text-lg text-white baseline">Contact</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="px-4">
+            <div className="pb-2">
+              <p className="font-semibold text-base md:text-lg text-white">Contact Details</p>
+            </div>
+
+            <div className="grid grid-cols-6 mb-2">
+              <div className="col-span-1">
+                <div className="flex align-middle">
+                  <ImLocation className="text-lg text-black mx-auto" />
+                </div>
               </div>
 
-              <div className="">
-                <div className="flex">
-                  <div className="mx-auto">
-                    <div className="py-2">
-                      <MdOutlineMyLocation className="text-lg text-white mx-auto" />
-                    </div>
+              <div className="col-span-5">
+                <p className="text-sm md:text-base text-black">
+                  #415, Padmavathy Paradise, <br />
+                  BEML Layout, Brookefield, <br />
+                  Bangalore, 560037
+                </p>
+              </div>
+            </div>
 
-                    <div className="">
-                      <p className="text-sm md:text-base text-black">
-                        #415, Padmavathy Paradise, <br />
-                        BEML Layout, Brookefield, <br />
-                        Bangalore, 560037
-                      </p>
-                    </div>
-                  </div>
+            <div className="grid grid-cols-6 my-2">
+              <div className="col-span-1">
+                <div className="flex align-middle">
+                  <MdEmail className="text-lg text-black mx-auto" />
                 </div>
+              </div>
 
-                <div className="flex">
-                  <div className="mx-auto">
-                    <div className="py-2">
-                      <MdEmail className="text-lg text-white mx-auto" />
-                    </div>
-
-                    <div className="">
-                      <p className="text-sm md:text-base text-black">
-                        shubhamraj465@gmail.com
-                      </p>
-                    </div>
-                  </div>
-                </div>
+              <div className="col-span-5">
+                <p className="text-sm md:text-base text-black">
+                  shubhamraj465@gmail.com
+                </p>
               </div>
             </div>
           </div>
 
-          <div className="w-1/3 md:w-1/2">
-            <div className="px-2">
-              <div>
-                <p className="mx-auto font-semibold text-center text-base md:text-lg text-white baseline">Other Projects</p>
+          <div className="px-4">
+            <div className="pb-2">
+              <p className="font-semibold text-base md:text-lg text-white">Other Projects to check out</p>
+            </div>
+
+            <div className="grid grid-cols-6 mb-2">
+              <div className="col-span-1">
+                <div className="flex align-middle">
+                  <MdWeb className="text-lg text-black mx-auto" />
+                </div>
               </div>
 
-              <div className="">
-                <div className="flex">
-                  <div className="mx-auto">
-                    <div className="py-2">
-                      <MdWeb className="text-lg text-white mx-auto" />
-                    </div>
+              <div className="col-span-5">
+                <a href="https://shubhamraj23.github.io/personal-portfolio" target="_blank" rel="noreferrer">
+                  <p className="text-sm md:text-base text-green-900 font-semibold">
+                    Personal Portfolio
+                  </p>
+                </a>
+              </div>
+            </div>
 
-                    <div className="">
-                      <a href="https://shubhamraj23.github.io/personal-portfolio" target="_blank" rel="noreferrer">
-                        <p className="text-sm md:text-base text-green-900 font-semibold text-center">
-                          Personal Portfolio
-                        </p>
-                      </a>
-                    </div>
-                  </div>
+            <div className="grid grid-cols-6 my-2">
+              <div className="col-span-1">
+                <div className="flex align-middle">
+                  <MdMeetingRoom className="text-lg text-black mx-auto" />
                 </div>
+              </div>
 
-                <div className="flex">
-                  <div className="mx-auto">
-                    <div className="py-2">
-                      <MdMeetingRoom className="text-lg text-white mx-auto" />
-                    </div>
-
-                    <div className="">
-                      <a href="https://shubhamraj23.github.io/escape-room" target="_blank" rel="noreferrer">
-                        <p className="text-sm md:text-base text-green-900 font-semibold text-center">
-                          Virtual Escape Room
-                        </p>
-                      </a>
-                    </div>
-                  </div>
-                </div>
+              <div className="col-span-5">
+                <a href="https://shubhamraj23.github.io/escape-room" target="_blank" rel="noreferrer">
+                  <p className="text-sm md:text-base text-green-900 font-semibold">
+                    Virtual Escape Room
+                  </p>
+                </a>
               </div>
             </div>
           </div>
