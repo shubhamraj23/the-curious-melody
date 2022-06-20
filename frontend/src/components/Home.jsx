5b4e79c470bdee3c0b11372afb75dcabd24dcb2e
Shubham Raj Pandit <shubhamraj465@gmail.com>
2022-06-20 20:34:17 +0530

    Changed color scheme

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import logo from '../media/logo.jpg'
+import logo from '../media/logo-transparent.png'
 import profilePic from '../media/profile-pic.jpg'
 
 const Home = () => {
@@ -32,14 +32,14 @@ const Home = () => {
 
   return (
     <div>
-      <div className="bg-shade-1">
+      <div className="bg-shade-2">
         <div className="flex flex-col items-center justify-center" style={{height: `${divHeight}px`}}>
           <img src={logo} className="w-1/2 md:w-1/3 block" alt="The Curious Melody Logo" />
-          <p className="text-white cassandra tagline-font">There is always something bigger hidden</p>
+          <p className="text-black dancing-script tagline-font">There is always something bigger hidden</p>
         </div>
       </div>
 
-      <div className="bg-shade-3">
+      <div className="bg-shade-1">
         <div className="py-8">
           <div className="grid grid-cols-1 md:grid-cols-2">
             <div className="mx-4">
@@ -53,7 +53,7 @@ const Home = () => {
         </div>
       </div>
 
-      <div className="bg-shade-2">
+      <div className="bg-shade-3">
         <div className="flex flex-row items-center md:justify-center" style={{height: `${boxHeight}`}}>
           <div className="grid grid-cols-1 md:grid-cols-3 my-16">
             <div className="w-1/2 mx-auto profile-width" style={{marginTop: `${imageMargin}px`}}>
@@ -62,14 +62,14 @@ const Home = () => {
 
             <div className="md:col-span-2 h-full">
               <div style={{marginTop: `${marginTop}px`}}>
-                <p className="text-left patrick-hand text-lg md:text-2xl text-white mx-4 md:mx-8" id="profile-description">
+                <p className="text-left patrick-hand text-lg md:text-2xl text-black mx-4 md:mx-8" id="profile-description">
                   Hello, and welcome to 'The Curious Melody'. <br/>
                   My name is Shubham, and I am a tech-enthusiast. Writing is one of my old hobbies. It started as an
                   activity to jot down my thoughts, and following the process over the years, I now have a collection
                   I would like to share. Thank you for stopping by, and I hope you enjoy the site. To know more about me,
                   checkout my&nbsp;
                   <a href="https://shubhamraj23.github.io/personal-portfolio" target="_blank" rel="noreferrer">
-                    <span className="hover:text-black">
+                    <span className="hover:text-white">
                       personal portfolio.
                     </span>
                   </a>
