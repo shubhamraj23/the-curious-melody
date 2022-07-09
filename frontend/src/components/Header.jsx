3fc60d1c9457528245c2185fa5178981ee5443f4
Shubham Raj Pandit <shubhamraj465@gmail.com>
2022-07-09 21:02:59 +0530

    Updated the header to fix itself when it reaches the top

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,23 +4,49 @@ import { HiMenu } from "react-icons/hi";
 
 const Header = () => {
   const [headerHeight, setHeight] = useState('100vh')
+  const [fixedHeight, setFixedHeight] = useState('10vh')
+  const [visibility, setVisibility] = useState('hidden')
 
   useEffect(() => {
     const width = window.innerWidth
     const height = window.innerHeight
     const finalHeight = Math.min(1, width/height)*100
     setHeight(`${finalHeight}vh`)
+
+    const fixedIcon = document.getElementById('side-toggle')
+    const headerHeight = fixedIcon.offsetHeight + 2*fixedIcon.getBoundingClientRect().top
+    setFixedHeight(`${headerHeight}px`)
   }, [])
+
+  useEffect(() => {
+    const changeVisibility = () => {
+      const fullHeader = document.getElementById('full-header')
+      const headerPosition = fullHeader.getBoundingClientRect().bottom
+      const minHeight = parseInt(fixedHeight.slice(0, fixedHeight.length - 2), 10)
+      if (headerPosition <= minHeight) {
+        setVisibility('')
+      }
+      else {
+        setVisibility('hidden')
+      }
+    }
+
+    window.addEventListener('scroll', changeVisibility, {passive: true})
+    return () => window.removeEventListener('scroll', changeVisibility)
+  }, [fixedHeight])
 
   return (
     <div className="bg-color-1">
-      <div className="fixed top-2.5 left-2.5 md:top-5 md:left-5">
+      <div className="fixed top-2.5 left-2.5 md:top-5 md:left-5 z-20" id="side-toggle">
         <HiMenu className="text-color-2 text-2xl md:text-3xl hover:cursor-pointer" />
       </div>
       
-      <div className="flex flex-col items-center justify-center py-8 md:py-12" style={{minHeight: `${headerHeight}`}}>
+      <div className="flex flex-col items-center justify-center py-8 md:py-12" style={{minHeight: `${headerHeight}`}} id="full-header">
         <img src={Logo} className="w-1/2 md:w-1/3 block" alt="The Curious Melody Logo" />
         <p className="text-color-2 dancing-script tagline-font mt-2">There is always something bigger hidden</p>
+      </div>
+
+      <div className={`bg-color-1 fixed top-0 w-full z-10 ${visibility}`} style={{height: `${fixedHeight}`}}>
       </div>
     </div>
   )
