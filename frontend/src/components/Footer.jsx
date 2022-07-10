ee1551dc636e291da94103701f64f16dc23953f9
Shubham Raj Pandit <shubhamraj465@gmail.com>
2022-07-10 13:51:54 +0530

    Added footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,53 @@
+import { MdWeb, MdMeetingRoom } from "react-icons/md";
+
+const Footer = () => {
+  return (
+    <div className="bg-color-1">
+      <div className="py-4">
+        <div className="px-4">
+          <div className="pb-2">
+            <p className="font-semibold text-base md:text-lg text-color-3">Other Projects to check out</p>
+          </div>
+
+          <div className="grid grid-cols-6 md:grid-cols-12 mb-2">
+            <div className="col-span-1">
+              <div className="flex align-middle">
+                <MdWeb className="text-lg text-color-2 mx-auto" />
+              </div>
+            </div>
+
+            <div className="col-span-5">
+              <a href="https://shubhamraj23.github.io/personal-portfolio" target="_blank" rel="noreferrer">
+                <p className="text-sm md:text-base text-color-2 font-semibold hover:text-white">
+                  Personal Portfolio
+                </p>
+              </a>
+            </div>
+          </div>
+
+          <div className="grid grid-cols-6 md:grid-cols-12 my-2">
+            <div className="col-span-1">
+              <div className="flex align-middle">
+                <MdMeetingRoom className="text-lg text-color-2 mx-auto" />
+              </div>
+            </div>
+
+            <div className="col-span-5">
+              <a href="https://shubhamraj23.github.io/escape-room" target="_blank" rel="noreferrer">
+                <p className="text-sm md:text-base text-color-2 font-semibold hover:text-white">
+                  The Lost City of Alexandra
+                </p>
+              </a>
+            </div>
+          </div>
+
+          <div>
+            <p className="text-sm text-white">&#169; Shubham Raj Pandit</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Footer
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -16,14 +16,11 @@ const Header = () => {
     const fixedIcon = document.getElementById('side-toggle')
     const headerHeight = fixedIcon.offsetHeight + 2*fixedIcon.getBoundingClientRect().top
     setFixedHeight(`${headerHeight}px`)
-  }, [])
 
-  useEffect(() => {
     const changeVisibility = () => {
-      const fullHeader = document.getElementById('full-header')
-      const headerPosition = fullHeader.getBoundingClientRect().bottom
-      const minHeight = parseInt(fixedHeight.slice(0, fixedHeight.length - 2), 10)
-      if (headerPosition <= minHeight) {
+      const tagline = document.getElementById('tagline')
+      const taglinePosition = tagline.getBoundingClientRect().bottom
+      if (taglinePosition <= 0) {
         setVisibility('')
       }
       else {
@@ -33,7 +30,7 @@ const Header = () => {
 
     window.addEventListener('scroll', changeVisibility, {passive: true})
     return () => window.removeEventListener('scroll', changeVisibility)
-  }, [fixedHeight])
+  }, [])
 
   return (
     <div className="bg-color-1">
@@ -41,9 +38,9 @@ const Header = () => {
         <HiMenu className="text-color-2 text-2xl md:text-3xl hover:cursor-pointer" />
       </div>
       
-      <div className="flex flex-col items-center justify-center py-8 md:py-12" style={{minHeight: `${headerHeight}`}} id="full-header">
+      <div className="flex flex-col items-center justify-center py-8 md:py-12" style={{minHeight: `${headerHeight}`}}>
         <img src={Logo} className="w-1/2 md:w-1/3 block" alt="The Curious Melody Logo" />
-        <p className="text-color-2 dancing-script tagline-font mt-2">There is always something bigger hidden</p>
+        <p className="text-color-2 dancing-script tagline-font mt-2" id="tagline">There is always something bigger hidden</p>
       </div>
 
       <div className={`bg-color-1 fixed top-0 w-full z-10 ${visibility}`} style={{height: `${fixedHeight}`}}>
