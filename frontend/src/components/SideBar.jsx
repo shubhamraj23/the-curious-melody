45043f0f6d54154b4192a5bae9c78302ade8db04
Shubham Raj Pandit <shubhamraj465@gmail.com>
2022-07-15 19:19:39 +0530

    Added content to the side bar

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,10 +1,21 @@
 import { ImCross } from "react-icons/im";
+import Logo from '../media/profile-image.jpg'
 
 const SideBar = ({sideState, changeState}) => {
   return (
     <div className={`fixed top-0 bg-color-2 w-3/4 md:w-1/2 h-screen z-30 ${sideState}`}>
       <div className="absolute top-3 right-3">
         <ImCross className="text-lg md:text-xl hover:cursor-pointer text-color-1" onClick={changeState}/>
+      </div>
+
+      <div className="mt-10">
+        <img src={Logo} className="mx-auto w-1/2 md:w-1/3 rounded-full mb-4" alt="The Curious Melody Logo" />
+        <p className="text-color-1 roboto italic side-tag">Look beneath the obvious.</p>
+        <p className="text-color-1 roboto italic side-tag">There is always something bigger hidden.</p>
+      </div>
+
+      <div className="my-6 flex justify-center">
+        <button className="profile-button roboto font-semibold">View Profile</button>
       </div>
     </div>
   )
