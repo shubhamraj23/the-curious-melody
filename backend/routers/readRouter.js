d2a1c8a39c7f596bb1748c61f04adae5695f6735
Shubham Raj Pandit <shubhamraj465@gmail.com>
2022-07-16 20:32:16 +0530

    Added routers to read the count of posts

--- backend/routers/readRouter.js
+++ backend/routers/readRouter.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const Post = require('../models/post')
 const Collection = require('../models/collection')
+const { postTypes } = require('../customization')
 const router = new express.Router()
 
 // Router to read a post
@@ -8,7 +9,7 @@ router.get('/post/:id', async (request, response) => {
   try {
     const post = await Post.findById(request.params.id)
     if (!post) {
-      response.status(400).send({
+      return response.status(400).send({
         error: "Invalid Post Id."
       })
     }
@@ -26,8 +27,8 @@ router.get('/collection/:id', async (request, response) => {
   try {
     const collection = await Collection.findById(request.params.id)
     if (!collection) {
-      response.status(400).send({
-        error: "Invalid Post Id."
+      return response.status(400).send({
+        error: "Invalid Collection Id."
       })
     }
 
@@ -44,6 +45,43 @@ router.get('/collection/:id', async (request, response) => {
     response.status(200).send(collectionPosts)
 
   } catch (e) {
+    response.status(500).send({
+      error: "Something unprecedented happened. Please try again."
+    })
+  }
+})
+
+// Router to count the number of posts of a specific type.
+router.get('/count/:type', async (request, response) => {
+  try {
+    const type = request.params.type
+    if (!postTypes.includes(type)){
+      return response.status(400).send({
+        error: "Invalid Post type."
+      })
+    }
+
+    const count = await Post.countDocuments({ type })
+    response.status(200).send({ [type]:count })
+
+  } catch (e) {
+    response.status(500).send({
+      error: "Something unprecedented happened. Please try again."
+    })
+  }
+})
+
+// Router to count the number of all types of posts.
+router.get('/countAll', async (request, response) => {
+  try {
+    let count = {}
+    for (const type of postTypes) {
+      const result = await Post.countDocuments({ type })
+      count = {...count, [type]:result}
+    }
+    response.status(200).send(count)
+
+  } catch(e) {
     response.status(500).send({
       error: "Something unprecedented happened. Please try again."
     })
